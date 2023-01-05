66b04af0265ae5178efe4bf6fe225d2177b9af8b
dibahadie <diba.hadie@gmail.com>
2023-01-05 12:55:13 +0330

    Final version of createfile

--- vim.c
+++ vim.c
@@ -2,6 +2,23 @@
 #include <dirent.h>
 #include <stdbool.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>
+#define MAX_SIZE 500
+
+int create_dir(char *path);
+int create_file(char *path);
+int get_command();
+int gnc(char** command, char** next_command, char** remaining);
+
+
+
+int main(){
+    mkdir("root", 0777);
+    while(1){
+        if(!get_command()) printf("invalid command\n");
+    }
+}
 
 int create_dir(char *path){
     char *remained_path = path;
@@ -19,9 +36,9 @@ int create_dir(char *path){
     else mkdir(path, 0777);
     return result;
 }
-
 int create_file(char *path){
     path = path + 1;
+    
     if(strchr(path, '/') == NULL){
         struct stat buff;
         if(stat(path, &buff) == -1){
@@ -34,15 +51,52 @@ int create_file(char *path){
     }else{
         char *filename = strrchr(path, '/');
         char *dirpath;
+        struct stat buff;
         dirpath = (char*) malloc(sizeof(path));
         strncpy(dirpath, path, strlen(path) - strlen(filename));
         create_dir(dirpath);
-        FILE *file = fopen(path, "w");
-        fclose(file);
-        return 1;
+        if(stat(path, &buff) == -1){
+            FILE *file = fopen(path, "w");
+            if(file == NULL){
+                printf("File creation failed.\n");
+                return 1;
+            }
+            fclose(file);
+            return 1;
+        }
+        else{
+            printf("The given file already exists\n");
+        }
+        return 0;
     }
 }
+int get_command(){
+    char *input;
+    input = (char*) malloc(sizeof(char) * MAX_SIZE);
+    fgets(input, MAX_SIZE, stdin);
+    char *initial_command, *remaining;
+    if(gnc(&input, &initial_command, &remaining) == 0) return 0;
 
-int main(){
-    mkdir("root", 0777);
+    if(!strcmp(initial_command, "createfile")){
+        char* filepath;
+        char* input_format;
+        if(gnc(&remaining, &input_format, &filepath) == 0) return 0;
+        if(strcmp(input_format, "--file")) return 0;
+        else{
+            create_file(filepath);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int gnc(char** command, char** next_command, char** remaining){
+    *remaining = (char*) malloc(sizeof(*command));
+    *next_command = (char*) malloc(sizeof(*command));
+    char* a = strchr(*command, ' ');
+    if(a == NULL) return 0;
+    *remaining = a;
+    strncpy(*next_command, *command, strlen(*command) - strlen(*remaining));
+    (*remaining) ++;
+    return 1;
 }
