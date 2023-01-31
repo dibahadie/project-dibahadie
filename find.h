046969a815de36f2cc5e9d1d6f1564d1d9269247
dibahadie <diba.hadie@gmail.com>
2023-01-31 13:42:22 +0330

    wildcard function completed

--- find.h
+++ find.h
@@ -8,14 +8,84 @@
 #define MAX_FILE 10000
 
 char* path_validation(char initial[]);
+int existance_validation(char path[]);
+int get_input(char* command, char** text, char* next_identifier, char*pre_identifier);
+
+char *strstr_wild(char *str, char *to_be_found){
+    char *ptr = strchr(to_be_found, '*');
+    if(ptr == NULL) {
+        return strstr(str, to_be_found);
+    }
+    else{
+        if(to_be_found[0] == '*'){
+            to_be_found ++;
+            ptr = strstr(str, to_be_found);
+            if(ptr == NULL) return NULL;
+            int index = strlen(str) - strlen(ptr);
+            while(str[index] != EOF && str[index] != '\0' && str[index] != ' ' && str[index] != '\n'){
+                ptr--;
+                index--;
+            }
+            return ++ptr;
+        }
+        else if(to_be_found[strlen(to_be_found) - 1] == '*' && to_be_found[strlen(to_be_found) - 1] != '\\'){
+            char *new;
+            new = (char*) malloc(sizeof(to_be_found));
+            strncpy(new, to_be_found, strlen(to_be_found) - 1);
+            ptr = strstr(str, new);
+            if(ptr == NULL) return NULL;
+            int index = strlen(str) - strlen(ptr) + strlen(new);
+            if(str[index] == '\0' || str[index] == EOF || str[index] == '\n') return NULL;
+            return ptr;
+        }
+        else if(to_be_found[strlen(to_be_found) - 1] == '*' && to_be_found[strlen(to_be_found) - 1] == '\\'){
+            int size = strlen(to_be_found);
+            char *new;
+            new = (char*) malloc(sizeof(to_be_found));
+            strcpy(new, to_be_found);
+            for(int i=0; i<size - 1; i++){
+                if(new[i] == '\\' && new[i+1] == '*'){
+                    for(int j=i; j<size; j++) new[j] = new[j+1];
+                    size --;
+                    new[i] = '*';
+                }
+            }
+            return strstr(str, new);
+        }
+        else{
+            char *first_part = (char*) malloc(sizeof(to_be_found));
+            char *second_part = (char*) malloc(sizeof(to_be_found));
+            strncpy(first_part, to_be_found, strlen(to_be_found) - strlen(ptr));
+            strcpy(second_part, to_be_found + strlen(first_part) + 2);
+
+            if(first_part[strlen(first_part) - 1] != ' '){
+                char *find = strstr(str, first_part);
+                char *ptr_tmp = find;
+                ptr_tmp += strlen(first_part);
+                if(*(ptr_tmp) == ' ' || *(ptr_tmp) == EOF || *(ptr_tmp) == '\0') return NULL;
+                else{
+                    while(*ptr_tmp != ' ' && *ptr_tmp != '\0' && *ptr_tmp != EOF){
+                        ptr_tmp++;
+                    }
+                    ptr_tmp++;
+                    int second_size = strlen(to_be_found) - strlen(first_part) - 2;
+                    for(int i=0; i<second_size; i++){
+                        if(*ptr_tmp != second_part[i]) return strstr_wild(ptr_tmp, to_be_found);
+                        ptr_tmp++;
+                    }
+                    return find;
+                }
+            }
+            return NULL;
+        }
+    }
+}
 
 int find_first_index(char *filepath, char *str){
     filepath = path_validation(filepath);
+    int r = existance_validation(filepath);
+    if(r != 1) return r;
     FILE *file = fopen(filepath, "r");
-    if(file == NULL){
-        printf("The given file doesn't exist\n");
-        return -1;
-    }
     char c = fgetc(file);
     char *content;
     content = (char*) malloc(sizeof(char) * MAX_FILE);
@@ -23,7 +93,7 @@ int find_first_index(char *filepath, char *str){
         content[strlen(content)] = c;
         c = fgetc(file);
     }
-    char *loc_ptr = strstr(content, str);
+    char *loc_ptr = strstr_wild(content, str);
     if(loc_ptr == NULL){
         printf("Expression not found\n");
         return -1;
--- vim.c
+++ vim.c
@@ -51,27 +51,27 @@ int get_command(){
     
     else if(!strcmp(initial_command, "find")){
         char *filepath, *str;
-        if(!get_input(input, &str, " --file", "--str ")) return 0;
+        if(!get_input(input, &str, " --file", "--str ")) return -105;
         string_validation(str);
         char *count_ptr = strstr(input, "-count");
         char *all_ptr = strstr(input, "-all");
         char *byword_ptr = strstr(input, "-byword");
         char *at_ptr = strstr(input, "-at");
         if(count_ptr == NULL && all_ptr == NULL && byword_ptr == NULL && at_ptr == NULL){
-            if(!get_input(input, &filepath, "\n", "--file ")) return 0;
+            if(!get_input(input, &filepath, "\n", "--file ")) return -105;
             printf("%d\n", find_first_index(filepath, str));
             return 1;
         }
         if(all_ptr == NULL && byword_ptr == NULL && at_ptr == NULL){
-            if(!get_input(input, &filepath, " -count", "--file ")) return 0;
+            if(!get_input(input, &filepath, " -count", "--file ")) return -105;
             printf("number of occurrence : %d\n", find_count(filepath, str));
             return 1;
         }
         if(count_ptr == NULL && all_ptr == NULL && byword_ptr == NULL){
             char *n_str;
             int n;
-            if(!get_input(input, &filepath, " -at", "--file ")) return 0;
-            if(!get_input(input, &n_str, "\n", "-at ")) return 0;
+            if(!get_input(input, &filepath, " -at", "--file ")) return -105;
+            if(!get_input(input, &n_str, "\n", "-at ")) return -105;
             n = atoi(n_str);
             printf("%d\n", find_at(filepath, str, n));
             return 1;
@@ -79,14 +79,14 @@ int get_command(){
     }
     else if(!strcmp(initial_command, "auto-indent")){
         char *filepath;
-        if(!get_input(input, &filepath, "\n", "--file ")) return 0;
+        if(!get_input(input, &filepath, "\n", "--file ")) return -105;
         auto_indent(filepath);
         return 1;
     }
 
     else if(!strcmp(initial_command, "compare")){
         char *filepath1, *filepath2;
-        if(!get_input(input, &filepath1, "\n", "compare ")) return 0;
+        if(!get_input(input, &filepath1, "\n", "compare ")) return -105;
         filepath2 = strchr(filepath1 + 1, ' ');
         filepath2 ++;
         filepath1[strlen(filepath1) - 1 - strlen(filepath2)] = '\0';
@@ -98,7 +98,7 @@ int get_command(){
     else if(!strcmp(initial_command, "tree")){
         char *depth_str;
         int depth;
-        if(!get_input(input, &depth_str, "\n", "tree ")) return 0;
+        if(!get_input(input, &depth_str, "\n", "tree ")) return -105;
         depth = atoi(depth_str);
         printf("%s", tree("root", 0, depth));
         return 1;
@@ -116,7 +116,6 @@ int get_command(){
         return run_paste(input);
     }
 
-    
-    return 0;
+    return -105;
 }
 
